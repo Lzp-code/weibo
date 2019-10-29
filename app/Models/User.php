02ce1847a7848f0398ac747114d69b6f53970c44
Lzp-code <lu-zhipeng@foxmail.com>
2019-10-29 16:04:31 +0000

    6.3-signup-page

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,8 +39,11 @@ class User extends Authenticatable
 
 
     public function gravatar($size = '100'){
-        $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "heet://www.gravatar.com/avatar/$hash?s=$size";
+//        $hash = md5(strtolower(trim($this->attributes['email'])));
+//        return "http://www.gravatar.com/avatar/$hash?s=$size";
+
+        return "http://weibo.test/headimg.jpeg";
+
     }
 
 
