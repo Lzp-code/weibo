23c61fd575620cadf75f5ffe4cebbed8151501d5
Lzp-code <lu-zhipeng@foxmail.com>
2019-11-03 17:23:21 +0000

    8.2-update-user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -24,18 +24,38 @@ class UsersController extends Controller
             'email'=>'required|email|unique:users|max:255',//针对数据表users的唯一email验证
             'password'=>'required|confirmed|min:6'//最少六位，要求两次输入的密码要一致
         ]);
-
-
         $user = User::create([
            'name'=>$request->name,
             'email'=>$request->email,
             'password'=>bcrypt($request->password),
         ]);
-
         Auth::login($user);
         session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show',[$user]);
+    }
 
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+
+
+    public function update(User $user,Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+
+       session()->flash('success','个人资料更新成功！');
+
+        return redirect()->route('users.show',$user);
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,13 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('signup','UsersController@create')->name('signup');
+
 Route::resource('users', 'UsersController');
+//resource 方法来为用户添加好完整的 RESTful 动作，因此我们不需要再为用户添加编辑页面的路由。但你需要知道，一个符合 RESTful 架构的用户编辑路由应该是像下面这样：
 //Route::get('/users/{user}','UsersController@show')->name('users.show');
+//Route::get('/users/{user}/edit','UsersController@edit')->name('users.edit');
 
 Route::get('login','SessionsController@create')->name('login');
 Route::post('login','SessionsController@store')->name('login');
 Route::delete('logout','SessionsController@destory')->name('logout');
+
